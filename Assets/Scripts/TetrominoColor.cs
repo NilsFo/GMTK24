13b2b4d8d1e4f9c66f5d0e1a280c616aa4682a7e
Nils Fo <nils.foerster@yahoo.de>
2024-08-17 18:14:59 +0200

    Updated materials and color

---
+++ Assets/Scripts/TetrominoColor.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TetrominoGroupBase))]
+public class TetrominoColor : MonoBehaviour
+{
+
+private GameState _gameState;
+
+    [SerializeField]
+    private List<MeshRenderer> meshRenderers;
+    private TetrominoGroupBase _tetromino;
+
+    void Awake()
+    {
+        _tetromino = GetComponent<TetrominoGroupBase>();
+        _gameState = FindObjectOfType<GameState>();
+        meshRenderers = new List<MeshRenderer>();
+        meshRenderers.AddRange(GetComponentsInChildren<MeshRenderer>());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_tetromino.Type == Tetromino.TetrominoType.Scaffold)
+        {
+            NextPaint();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NextPaint()
+    {
+        var mat = NextColor();
+        foreach (var renderer in meshRenderers)
+        {
+            renderer.material = mat;
+        }
+    }
+
+    
+    private Material NextColor()
+    {
+        return _gameState.tetrominoScafoldingMaterials[UnityEngine.Random.Range(0, _gameState.tetrominoScafoldingMaterials.Count - 1)];
+    }
+
+}
