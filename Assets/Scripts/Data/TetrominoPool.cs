3bb5cd8d2253bf387ab934909116ddc49f5b5f18
Nils Fo <nils.foerster@yahoo.de>
2024-08-17 11:15:33 +0200

    Basic Spawning Methodology

--- Assets/Scripts/Data/TetrominoPool.cs
+++ Assets/Scripts/Data/TetrominoPool.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 using System;
 
 [CreateAssetMenu(fileName = "New Tetromino Pool", menuName = "GMTK24/ScriptableObjects/New Tetromino Pool", order = 1)]
-public class SpawnManagerScriptableObject : ScriptableObject
+public class TetrominoPool : ScriptableObject
 {
 
-
     public List<TetrominoPoolEntry> tetrominos;
+
+    public TetrominoGroupBase Next()
+    {
+        List<TetrominoGroupBase> tetrominoCandidates = new List<TetrominoGroupBase>();
+
+        foreach (TetrominoPoolEntry entry in tetrominos)
+        {
+            tetrominoCandidates.Add(entry.tetromino);
+        }
+
+        return tetrominoCandidates[UnityEngine.Random.Range(0, tetrominoCandidates.Count - 1)];
+    }
 
     [Serializable]
     public class TetrominoPoolEntry
--- Assets/Scripts/TetrominoSpawner.cs
+++ Assets/Scripts/TetrominoSpawner.cs
@@ -6,18 +6,34 @@ public class TetrominoSpawner : MonoBehaviour
 {
 
 
-    
+    public TetrominoPool tetrominoPool;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Next();
+        }
     }
+
+    public GameObject Next()
+    {
+        TetrominoGroupBase element = tetrominoPool.Next();
+
+        GameObject newElement = Instantiate(element.gameObject, transform.position, Quaternion.identity);
+        newElement.transform.position = transform.position;
+        newElement.transform.parent = null;
+
+        return newElement;
+    }
+
+
 }
