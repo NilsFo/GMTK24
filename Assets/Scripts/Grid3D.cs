b0c9fb669450bb8afd6b6ceeafc8c2d89a5bd1cd
PatrickQ <Patrick@Quiskamp.de>
2024-08-17 10:56:41 +0200

    Grid Done

--- Assets/Scripts/Grid3D.cs
+++ Assets/Scripts/Grid3D.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Grid3D : MonoBehaviour
 {
 
-    private GameObject[,,] _grid;
+    private TetrominoGroupBase[,,] _grid;
 
     [SerializeField] private int sizeX = 10;
     [SerializeField] private int sizeY = 10;
@@ -19,28 +20,103 @@ public class Grid3D : MonoBehaviour
 
     private void Awake()
     {
-        _grid = new GameObject[sizeX, sizeY, sizeZ];
+        _grid = new TetrominoGroupBase[sizeX, sizeY, sizeZ];
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private (int, bool) ToIndex(float pos, int minX, int maxX)
     {
-        
+        bool isNotInside = false;
+        float reminderX = pos % blockScaleX;
+        int index = (int)((pos - reminderX) / blockScaleX);
+        if(reminderX < 0) index += 1;
+        if (index < minX)
+        {
+            index = minX;
+            isNotInside = true;
+        }
+        if(index > maxX)
+        {
+            index = maxX;
+            isNotInside = true;
+        }
+        return (index, isNotInside);
     }
 
-    // Update is called once per frame
-    void Update()
+    private float ToWorld(int index, float scale)
     {
-        
-    }
-
-    public int[] WorldToLocal(Vector3 pos)
-    {
-        return new[] { 0, 0, 0 };
+        return index * scale;
     }
     
-    public Vector3 LocalToWorld(int[] index)
+    public Vector3Int WorldToLocal(Vector3 pos)
     {
-        return Vector3.zero;
+        var indexX = ToIndex(pos.x, 0, sizeX);
+        var indexY = ToIndex(pos.y, 0, sizeY);
+        var indexZ = ToIndex(pos.z, 0, sizeZ);
+
+        return new Vector3Int(indexX.Item1, indexY.Item1, indexZ.Item1);
+    }
+    
+    public Vector3 LocalToWorld(Vector3Int pos)
+    {
+        float posX = ToWorld(pos.x, blockScaleX);
+        float posZ = ToWorld(pos.z, blockScaleZ);
+        float posY = ToWorld(pos.y, blockScaleY);
+
+        return new Vector3(posX, posY, posZ);
+    }
+
+    public bool IsInsideGrid(Vector3 pos)
+    {
+        var indexX = ToIndex(pos.x, 0, sizeX);
+        var indexY = ToIndex(pos.y, 0, sizeY);
+        var indexZ = ToIndex(pos.z, 0, sizeZ);
+
+        return !indexX.Item2 && !indexY.Item2 && !indexZ.Item2;
+    }
+
+    public Vector3Int[] ConvertToLocal(Vector3[] pos)
+    {
+        Vector3Int[] result = new Vector3Int[pos.Length];
+        for (int i = 0; i < pos.Length; i++) 
+        {
+            result[i] = WorldToLocal(pos[i]);
+        }
+        return result;
+    }
+
+    public Vector3[] ConvertToWorld(Vector3Int[] pos)
+    {
+        Vector3[] result = new Vector3[pos.Length];
+        for (int i = 0; i < pos.Length; i++) 
+        {
+            result[i] = LocalToWorld(pos[i]);
+        }
+        return result;
+    }
+    
+    public bool CanBePlaced(Vector3Int[] indexes)
+    {
+        bool result = true;
+        for (int i = 0; i < indexes.Length; i++)
+        {
+            var shape = _grid[indexes[i].x, indexes[i].y, indexes[i].z];
+            if (shape == null)
+            {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public bool PlaceShape(TetrominoGroupBase shape, Vector3Int[] indexes)
+    {
+        bool canBePlaced = CanBePlaced(indexes);
+        if (!canBePlaced) return false;
+        for (int i = 0; i < indexes.Length; i++)
+        {
+            _grid[indexes[i].x, indexes[i].y, indexes[i].z] = shape;
+        }
+        return true;
     }
 }
